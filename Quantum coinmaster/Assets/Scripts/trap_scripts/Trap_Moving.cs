f411610f62edb1d97e45b0983d5f73230e14b829
abbsimmei <simon.meier@abbgymnasiet.se>
2023-10-24 12:58:17 +0200

    Pyramid Level Done

--- Quantum coinmaster/Assets/Scripts/trap_scripts/Trap_Moving.cs
+++ Quantum coinmaster/Assets/Scripts/trap_scripts/Trap_Moving.cs
@@ -8,6 +8,11 @@ public class Trap_Moving : MonoBehaviour
 
     public float speed;
 
+    public float yBottom;
+    public float yTop;
+
+    private bool goingUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +22,25 @@ public class Trap_Moving : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetPosition = new Vector3(trap.position.x + speed, trap.position.y, trap.position.z);
+        Vector3 targetPosition = trap.position; // Declare targetPosition outside
 
-        trap.position = targetPosition;
+        if (trap.position.y > yBottom - speed && !goingUp) // Typo fix: goingUp instead of going
+        {
+            targetPosition = new Vector3(trap.position.x, trap.position.y - speed, trap.position.z);
+            if (trap.position.y < yBottom)
+            {
+                goingUp = true;
+            }
+        }
+        else if (trap.position.y < yTop + speed && goingUp) // Typo fix: goingUp instead of going
+        {
+            targetPosition = new Vector3(trap.position.x, trap.position.y + speed, trap.position.z);
+            if (trap.position.y > yTop)
+            {
+                goingUp = false;
+            }
+        }
 
+        trap.position = targetPosition; // Apply the targetPosition
     }
 }
