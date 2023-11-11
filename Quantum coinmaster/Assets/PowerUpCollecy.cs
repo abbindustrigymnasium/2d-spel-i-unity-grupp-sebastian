4d0f4ed827382ff3c55ae103ce00da2faaca1f64
sebastian200 <kotte.alin@gmail.com>
2023-11-11 09:25:18 +0100

    added powerup collectable

--- Quantum coinmaster/Assets/PowerUpCollecy.cs
+++ Quantum coinmaster/Assets/PowerUpCollecy.cs
@@ -9,30 +9,45 @@ public class powerUpCollect : MonoBehaviour
 
 public Player player;
 
-public bool isPowerUpCollected= false;
 
-public int powerUp;
+
+public int powerUpId = 0;
+
+public PowerUps powerUp;
 
     void start() {
-        if(isPowerUpCollected) {
-            Destroy(gameObject);
-        }
+
     }
 
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if(collider2D.gameObject.CompareTag("Player")) {
-            if(!isPowerUpCollected) {
+       
+          
                
-                Destroy(gameObject);
-                isPowerUpCollected = true;
+                
 
-            }
+                if (powerUpId == 1) {
+                    powerUp.superDrugPowerUpOn = true;
+                }
+                else if (powerUpId == 2)  {
+                    powerUp.invisibilityPowerUpOn = true;
+                }
+                else if  (powerUpId == 3) {
+                    powerUp.doubleJumpingPowerUpOn = true;
+                }
+                else if (powerUpId == 4)  {
+                    powerUp.moonGravityPowerUpOn = true;
+                }
+                else if (powerUpId == 5)  {
+                    powerUp.flyingPowerUpOn = true;
+                }
             
+        
 
-        }
+        
         Debug.Log("powerUpCollecteds");
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
